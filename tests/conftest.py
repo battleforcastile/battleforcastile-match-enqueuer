05f0c14ad5fb96e08f87a47389a43a160561884c
José Vidal <javidgon@gmail.com>
2019-08-26 23:33:07 +0200

    Add .circleci/config.yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,6 @@
-import json
-
 import pytest
-from flask_bcrypt import generate_password_hash
 
-from battleforcastile_auth import create_app, db
-from battleforcastile_auth.constants import BCRYPT_LOG_ROUNDS
-from battleforcastile_auth.models import User
+from battleforcastile_match_enqueuer import create_app
 
 
 @pytest.fixture(scope='function')
@@ -23,40 +18,3 @@ def test_client():
     yield testing_client  # this is where the testing happens!
 
     ctx.pop()
-
-
-@pytest.fixture(scope='function')
-def init_database():
-    flask_app = create_app('testing_config.py')
-
-    with flask_app.app_context():
-        from battleforcastile_auth.models import User
-
-        # Create the database and the database table
-        db.create_all()
-
-        yield db  # this is where the testing happens!
-
-        db.drop_all()
-
-
-@pytest.fixture(scope='function')
-def user1():
-    user = User(
-        email='user1@example.com',
-        username='user1',
-        password=(generate_password_hash('12345'.encode('utf-8'), BCRYPT_LOG_ROUNDS)).decode('utf-8'),
-        token='1111111111111111111111111111111111111111'
-    )
-    return user
-
-
-@pytest.fixture(scope='function')
-def user2():
-    user = User(
-        email='user2@example.com',
-        username='user2',
-        password=(generate_password_hash('12345'.encode('utf-8'), BCRYPT_LOG_ROUNDS)).decode('utf-8'),
-        token='1111111111111111111111111111111111111111'
-    )
-    return user
